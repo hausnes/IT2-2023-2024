9d5226a393a250b8186519e9612f2f396d2d2bd8
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-05-30 15:45:42 +0200

    OOP-eks om planetar

---
+++ oop/planetar.py
@@ -0,0 +1,14 @@
+class Planet:
+    def __init__(self, navn, solavstand, radius):
+        self.navn = navn
+        self.solavstand = solavstand
+        self.radius = radius
+
+mars = Planet("Mars", 227.9, 3389.5)
+jupiter = Planet("Jupiter", 778.5, 69911)
+
+jupiter.radius = 20000 # Endre radiusen til Jupiter
+print(jupiter.radius)
+
+# Kan printe ut kva datatype det er
+print(type(mars))
