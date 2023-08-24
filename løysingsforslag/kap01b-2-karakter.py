7746a2632bd4ce06892815a04069d24f8153ec17
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-08-24 23:11:15 +0200

    Create kap01b-2-karakter.py

---
+++ løysingsforslag/kap01b-2-karakter.py
@@ -0,0 +1,44 @@
+'''
+try:
+    poengsumElev = int(input("Skriv inn poengsum: "))
+    
+    if  poengsumElev >= 100 or poengsumElev < 0:
+        print("Ugyldig poengsum!")
+    elif poengsumElev >= 91:
+        karakter = 6
+    elif poengsumElev >= 81:
+        karakter = 5
+    elif poengsumElev >= 61:
+        karakter = 4
+    elif poengsumElev >= 41:
+        karakter = 3
+    elif poengsumElev >= 21:
+        karakter = 2
+    else:
+        karakter = 1
+
+    print(f"Karakteren til eleven er {karakter}.")
+
+except ValueError: # Hvis konverteringen mislykkast, gjer ein feilmelding
+    print("Du må skrive inn eit tal.")
+
+'''
+
+poengsumElev = int(input("Skriv inn poengsum: "))
+    
+if  poengsumElev >= 100 or poengsumElev < 0:
+    print("Ugyldig poengsum!")
+elif poengsumElev >= 91:
+    karakter = 6
+elif poengsumElev >= 81:
+    karakter = 5
+elif poengsumElev >= 61:
+    karakter = 4
+elif poengsumElev >= 41:
+    karakter = 3
+elif poengsumElev >= 21:
+    karakter = 2
+else:
+    karakter = 1
+
+print(f"Karakteren til eleven er {karakter}.")
