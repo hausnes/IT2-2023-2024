727e9c9bff98cb6af49fcefae69bacbc7eeb5432
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-09-05 14:39:43 +0200

    Create land.py

---
+++ løysingsforslag/land.py
@@ -0,0 +1,9 @@
+land = [
+    {"Land" : "Norge"  , "Hovedstad" : "Oslo",      "Innbyggere": 5391369,  "Fylker": 18},
+    {"Land" : "Sverige", "Hovedstad" : "Stockholm", "Innbyggere": 10099265, "Fylker": 21}
+]
+
+for x in land:
+    country = x["Land"]
+    innbyggere = x["Innbyggere"]
+    print(f"Land: {country} har {x['Innbyggere']} innbyggere")
