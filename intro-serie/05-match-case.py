12fdd5eb7795a89e4e6db0baa47bec6bb33d8ba8
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-09-18 20:59:04 +0200

    match - case ("switch")

--- intro-serie/05-match-case.py
+++ intro-serie/05-match-case.py
@@ -6,16 +6,12 @@ lang = input("What's the programming language you want to learn? ")
 match lang:
     case "JavaScript":
         print("You can become a web developer.")
-
     case "Python":
         print("You can become a Data Scientist")
-
     case "PHP":
         print("You can become a backend developer")
-    
     case "Solidity":
         print("You can become a Blockchain developer")
-
     case "Java":
         print("You can become a mobile app developer")
     case _:
