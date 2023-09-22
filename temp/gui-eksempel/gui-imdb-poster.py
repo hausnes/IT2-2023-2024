e34b4583ce9f148a1a5598762c0060afe86b1b94
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-09-22 13:18:06 +0200

    Update gui-imdb-poster.py

--- temp/gui-eksempel/gui-imdb-poster.py
+++ temp/gui-eksempel/gui-imdb-poster.py
@@ -1,4 +1,3 @@
-# import the customtkinter and tkinter modules
 import customtkinter as ctk
 import tkinter as tk
 from PIL import Image
@@ -7,24 +6,22 @@ from PIL import Image
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("blue")
 
-# create a class that inherits from ctk.CTk
 class App(ctk.CTk):
-  # define the __init__ method
   def __init__(self):
-    # call the __init__ method of the parent class
     super().__init__()
-    # set the title and geometry of the window
-    self.title("My GUI")
-    self.geometry("400x600")
-    img = customtkinter.CTkImage(Image.open("poster.png"), size=(30, 30))
-    # create a label widget with the image object
-    label = ctk.CTkLabel(master=self, image=img)
-    # place the label widget at the top center of the window
-    label.grid(row=0, column=0, sticky="nsew")
-    # create a button widget with the text "Click me"
-    button = ctk.CTkButton(master=self, text="Click me")
-    # place the button widget at the bottom center of the window
-    button.grid(row=1, column=0, sticky="nsew")
+
+    self.title("Filmplakat!")
+    self.geometry("320x418")
+    
+    # Bilde
+    img = ctk.CTkImage(Image.open("poster.png"), size=(320, 388))
+    label = ctk.CTkLabel(master=self, image=img) # create a label widget with the image object
+    label.grid(row=0, column=0, sticky="nsew") # place the label widget at the top center of the window
+    label.configure(text="Jaws")
+    
+    # Knapp
+    button = ctk.CTkButton(master=self, text="Finn plakat")
+    button.grid(row=1, column=0, sticky="nsew") # place the button widget at the bottom center of the window
 
 # create an instance of the App class
 app = App()
