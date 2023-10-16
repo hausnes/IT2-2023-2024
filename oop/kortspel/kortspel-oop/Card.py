c81dbd7fc3d3ff573ca750c20d71f586d287fe36
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-10-16 12:19:23 +0200

    Kortspel i OOP- og prosedyre-versjon

---
+++ oop/kortspel/kortspel-oop/Card.py
@@ -0,0 +1,48 @@
+# Card class
+
+import pygame
+import pygwidgets
+
+class Card():
+
+    BACK_OF_CARD_IMAGE = pygame.image.load('images/BackOfCard.png')
+
+    def __init__(self, window, rank, suit, value):
+        self.window = window
+        self.rank = rank
+        self.suit = suit
+        self.cardName = rank + ' of ' + suit
+        self.value = value
+        fileName = 'images/' + self.cardName + '.png'
+        # Set some starting location; use setLoc below to change
+        self.images = pygwidgets.ImageCollection(window, (0, 0),
+                                {'front': fileName, 
+                                 'back': Card.BACK_OF_CARD_IMAGE}, 'back')
+
+    def conceal(self):
+        self.images.replace('back')
+
+    def reveal(self):
+        self.images.replace('front')
+
+    def getName(self):
+        return self.cardName
+
+    def getValue(self):
+        return self.value
+
+    def getSuit(self):
+        return self.suit
+
+    def getRank(self):
+        return self.rank
+
+    def setLoc(self, loc): # call the setLoc method of the ImageCollection
+        self.images.setLoc(loc)
+
+    def getLoc(self):  # get the location from the ImageCollection
+        loc = self.images.getLoc()
+        return loc
+
+    def draw(self):
+        self.images.draw()
---
+++ oop/kortspel/kortspel-oop/Deck.py
@@ -0,0 +1,71 @@
+# Deck class
+
+import random
+from Card import *
+
+class Deck():
+    SUIT_TUPLE = ('Diamonds', 'Clubs', 'Hearts', 'Spades')
+    # This dict maps each card rank to a value for a standard deck
+    STANDARD_DICT = {'Ace':1, '2':2, '3':3, '4':4, '5':5,
+                                  '6':6, '7':7, '8': 8, '9':9, '10':10,
+                                  'Jack':11, 'Queen':12, 'King':13}
+
+    def __init__(self, window, rankValueDict=STANDARD_DICT):
+        # rankValueDict defaults to STANDARD_DICT, but you can call 
+        # with a different dict, e.g., a special dict for Blackjack
+        self.startingDeckList = []
+        self.playingDeckList = []
+        for suit in Deck.SUIT_TUPLE:
+            for rank, value in rankValueDict.items():
+                oCard = Card(window, rank, suit, value)
+                self.startingDeckList.append(oCard)
+
+        self.shuffle()
+
+    def shuffle(self):
+        # Copy the starting deck and save it in the playing deck list
+        self.playingDeckList = self.startingDeckList.copy()
+        for oCard in self.playingDeckList:
+            oCard.conceal()
+        random.shuffle(self.playingDeckList)
+
+    def getCard(self):
+        if len(self.playingDeckList) == 0:
+            raise IndexError('No more cards')
+        # Pop one card off the deck and return it
+        oCard = self.playingDeckList.pop()  
+        return oCard
+
+    def returnCardToDeck(self, oCard):
+        # Put a card back into the deck
+        self.playingDeckList.insert(0, oCard)
+
+
+if __name__ == '__main__':
+    # Main code to test the Deck class
+
+    import pygame
+
+    # Constants
+    WINDOW_WIDTH = 100
+    WINDOW_HEIGHT = 100
+
+    pygame.init()
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+    oDeck = Deck(window)
+    for i in range(1, 53):
+        oCard = oDeck.getCard()
+        print('Name: ', oCard.getName(), '  Value:', oCard.getValue())
+
+        
+##    Optional code to show blackjack deck
+##    print('BlackJack Deck:')
+##    blackJackDict = {'Ace':1, '2':2, '3':3, '4':4, '5':5,
+##                 '6':6, '7':7, '8': 8, '9':9, '10':10,
+##                 'Jack':10, 'Queen':10, 'King':10}
+##    oBlackjackDeck = Deck(window, rankValueDict=blackJackDict)
+##
+##    for i in range(1, 53):
+##        oCard = oBlackjackDeck.getCard()
+##        print('Name: ', oCard.getName(), '  Value:', oCard.getValue())
