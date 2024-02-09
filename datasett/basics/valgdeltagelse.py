bedccb923e8222c152aa5d651c70e2395a57d85f
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2024-02-09 10:02:06 +0100

    Update valgdeltagelse.py

--- datasett/basics/valgdeltagelse.py
+++ datasett/basics/valgdeltagelse.py
@@ -7,10 +7,14 @@ filnavn = "valgdeltagelse.txt"
 
 with open(filnavn) as fil:
   for linje in fil:
-    linjeTemp = linje.rstrip().split(",")
-
+    linjeTemp = linje.rstrip().split(";")
+    print(linjeTemp)
     år.append(int(linjeTemp[0]))
-    deltagelse.append(float(linjeTemp[1]))
+    deltagelse.append(float(linjeTemp[1].replace(",",".")))
 
 print(år)
 print(deltagelse)
+
+plt.plot(år,deltagelse)
+plt.grid()
+plt.show()
