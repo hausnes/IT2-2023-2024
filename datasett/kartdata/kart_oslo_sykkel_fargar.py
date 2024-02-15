badaabf30ecc38f7f84c14eac28099c169c9736d
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2024-02-15 21:51:21 +0100

    Kart med sykkeldata, binning, med/utan fargar

--- datasett/kartdata/kart_oslo_sykkel_fargar.py
+++ datasett/kartdata/kart_oslo_sykkel_fargar.py
@@ -2,6 +2,8 @@ import osmnx as ox
 import folium as fol
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
 # Les inn data
 df = pd.read_csv('sykkel.csv')
@@ -14,9 +16,16 @@ bins_lon = np.arange(df['end_station_longitude'].min(), df['end_station_longitud
 df['latbin'] = pd.cut(df['end_station_latitude'], bins=bins_lat)
 df['lonbin'] = pd.cut(df['end_station_longitude'], bins=bins_lon)
 binned = df.groupby(['latbin', 'lonbin']).size().reset_index(name='count')
+print(binned)
+
+# Normaliser fargene til antall punkter
+norm = colors.Normalize(binned['count'].min(), binned['count'].max())
 
 # Lager et kart
 m = fol.Map([59.911491, 10.757933], zoom_start=12) # Midtpunkt Oslo, og godt zooma ut
+
+# Velger fargekart
+cmap = plt.cm.viridis
 
 # Legger til boksene
 for _, row in binned.iterrows():
@@ -26,7 +35,11 @@ for _, row in binned.iterrows():
     # Bruk antall punkter i boksen til å bestemme radiusen av sirkelen
     # radius = row['count'] / 10
     radius = np.sqrt(row['count'])
-    fol.CircleMarker([lat, lon], radius=radius).add_to(m)
+    # Use the number of points in the bin to determine the color of the circle
+    color = cmap(norm(row['count']))
+    # Konverterer RGBA-fargar til HEX for bruk i folium
+    color = colors.to_hex(color)
+    fol.CircleMarker([lat, lon], radius=radius, color=color).add_to(m)
 
 # Lagrer kartet
-m.save("oslo_sykkel_binning.html")
+# m.save("oslo_sykkel_binning_fargar.html")
